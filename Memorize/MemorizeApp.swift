5a67d5613658111bcbf1bf5591e11390cfb81a6e
BlackFrog <blackfrog1@gmail.com>
2022-12-18 12:42:35 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Collin Bennett on 12/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
